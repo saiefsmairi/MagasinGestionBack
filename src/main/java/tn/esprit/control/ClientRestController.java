88c0348385f2eac0ffcdca02e4f913868ee48984
AmdouniBrahmiKhouloud <khouloud.amdounibrahmi@esprit.tn>
2021-11-28 19:56:52 +0100

    adding admin role to client

--- src/main/java/tn/esprit/control/ClientRestController.java
+++ src/main/java/tn/esprit/control/ClientRestController.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import tn.esprit.entity.CategorieClient;
 import tn.esprit.entity.Client;
 import tn.esprit.spring.service.IClientService;
 
+	@CrossOrigin(origins = "http://localhost:8089")
 	@RestController
 	@RequestMapping("/client")
 	public class ClientRestController { 
--- src/main/java/tn/esprit/entity/CategorieClient.java
+++ src/main/java/tn/esprit/entity/CategorieClient.java
@@ -1,5 +1,5 @@
 package tn.esprit.entity;
 
 public enum CategorieClient {
-fidele,ordinaire,premuim
+fidele,ordinaire,premuim,admin
 }
